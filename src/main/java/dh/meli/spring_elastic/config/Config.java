cf611935951c7c1c3639b724c9bcffeaca7eca6f
lecastroMELI <leticia.castro@mercadolivre.com>
2022-07-26 14:25:17 -0300

    Código config comentado

--- src/main/java/dh/meli/spring_elastic/config/Config.java
+++ src/main/java/dh/meli/spring_elastic/config/Config.java
@@ -10,19 +10,24 @@ import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
 import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
-// ANOTAÇÃO QUE DIZ AO SPIRNG QUE É UMA CLASSE DE CONFIGURAÇÃO. vale para qualquer configuração do SPRING.
+// ANOTAÇÃO QUE DIZ AO SPRING QUE É UMA CLASSE DE CONFIGURAÇÃO. (Vale para qualquer configuração do SPRING)
 @Configuration
-// NECESSÁRIO DEFINIR QUE É PARA O ELASTIC (caminho da pasta onde está o repository)
+// ANOTAÇÃO QUE DEFINE O REPOSITÓRIO QUE O ELASTIC DEVERÁ USAR (caminho da pasta onde está o repository)
 @EnableElasticsearchRepositories(basePackages = "dh.meli.spring_elastic.repository")
 @ComponentScan(basePackages = "dh.meli.spring_elastic")
 public class Config {
 
-    @Bean // DEFINE QUE O SPRING É O RESPONSÁVEL PELA MANIPULAÇÃO DESTA CONEXÃO
+    /* Anotação que usamos para indicar ao Spring que o método retornará um objeto a ser
+    * registrado como um Bean de contexto de aplicação. Ou seja:
+    * DEFINE QUE O SPRING É O RESPONSÁVEL PELA MANIPULAÇÃO DESTA CONEXÃO */
+    @Bean
+    /* É um client REST de alto nível para consumir Elasticsearch de Java. Usamos para registrar no Spring a
+    * configuração da conexão com a API Elasticsearch Rest. Também usado para operações CRUD no Elasticsearch */
     public RestHighLevelClient client() {
         ClientConfiguration clientConfiguration = ClientConfiguration.builder()
             // DEFINE QUAL É O SERVIDOR
             .connectedTo("localhost:9200")
-            // USUÁRIO E SENHA (SE TIVER)
+            // DEFINE USUÁRIO E SENHA (SE TIVER)
             // .withBasicAuth("elastc", "")
             // AJUSTA O TIMEOUT. POR PADRÃO SÃO 5 SEGUNDOS PARA CONECTAR
             .withConnectTimeout(10000)
@@ -33,7 +38,8 @@ public class Config {
         return RestClients.create(clientConfiguration).rest();
     }
 
-    // MÉTODO RESPONSÁVEL POR INFORMAR AO SPRING AS OPERAÇÕES
+    /* ElasticSearchOperations será responsável por executar as operações no servidor
+    * Elasticsearch e, neste caso, usará um ElasticsearchRestTemplate. */
     @Bean
     public ElasticsearchOperations elasticsearchTemplate() {
             return new ElasticsearchRestTemplate(client());
