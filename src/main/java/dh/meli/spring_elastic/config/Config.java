9d0744239863134c12d4f970eac4a1c573d9af08
lecastroMELI <leticia.castro@mercadolivre.com>
2022-07-26 09:43:04 -0300

    Criado arquivo de configuração da conexão

---
+++ src/main/java/dh/meli/spring_elastic/config/Config.java
@@ -0,0 +1,38 @@
+package dh.meli.spring_elastic.config;
+
+import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.client.RestClients;
+import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
+import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+
+// ANOTAÇÃO QUE DIZ AO SPIRNG QUE É UMA CLASSE DE CONFIGURAÇÃO. vale para qualquer configuração do SPRING.
+@Configuration
+// NECESSÁRIO DEFINIR QUE É PARA O ELASTIC (caminho da pasta onde está o repository)
+@EnableElasticsearchRepositories(basePackages = "dh.meli.spring_elastic.repository")
+public class Config {
+
+        @Bean // DEFINE QUE O SPRING É O RESPONSÁVEL PELA MANIPULAÇÃO DESTA CONEXÃO
+    public RestHighLevelClient client() {
+        ClientConfiguration clientConfiguration = ClientConfiguration.builder()
+            // DEFINE QUAL É O SERVIDOR
+            .connectedTo("localhost:9200")
+            // USUÁRIO E SENHA (SE TIVER)
+            // .withBasicAuth("elastc", "")
+            // AJUSTA O TIMEOUT. POR PADRÃO SÃO 5 SEGUNDOS PARA CONECTAR
+            .withConnectTimeout(10000)
+            .build();
+
+        // RETORNA A CONFIGURAÇÃO DO CLIENTE. ".rest" PARA CONVERTER PARA RestHighLevelClient
+        return RestClients.create(clientConfiguration).rest();
+    }
+
+    // MÉTODO RESPONSÁVEL POR INFORMAR AO SPRING AS OPERAÇÕES
+    @Bean
+    public ElasticsearchOperations elasticsearchOperations() {
+            return new ElasticsearchRestTemplate(client());
+    }
+}
