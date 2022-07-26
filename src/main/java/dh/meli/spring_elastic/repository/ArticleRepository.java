10579f4baf877411d85cb7582902268159f719b9
lecastroMELI <leticia.castro@mercadolivre.com>
2022-07-26 10:28:58 -0300

    Repository alterado

--- src/main/java/dh/meli/spring_elastic/repository/ArticleRepository.java
+++ src/main/java/dh/meli/spring_elastic/repository/ArticleRepository.java
@@ -2,8 +2,8 @@ package dh.meli.spring_elastic.repository;
 
 import dh.meli.spring_elastic.model.Article;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-import org.springframework.stereotype.Repository;
 
+// JÁ FOI DEFINIDO NA CONEXÃO QUE AQUI ESTÁ MEU REPOSITRY
 // RECEBE A CLASSE E O TIPO DA CHAVE ID
 public interface ArticleRepository extends ElasticsearchRepository<Article, Integer> {
 }
